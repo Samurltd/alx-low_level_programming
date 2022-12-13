a02652786182dc5142d724acd74336bdaad4ea55
Ben Sam <bensmoothsamel@gmail.com>
2022-12-12 17:56:59 -0800

    3. Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp https://github.com/Samurltd/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libright.so
-export LD_PRELOAD=/tmp/libright.so
+export LD_PRELOAD=/$PWD/libright.so ./gm 9 8 10 24 75 9
